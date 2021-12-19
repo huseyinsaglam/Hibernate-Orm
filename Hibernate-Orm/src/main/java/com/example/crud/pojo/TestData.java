cb870fc20dc8fddbc615f5f4d6534bfd59eee281
ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>
2021-12-19 19:28:32 +0300

    crud

---
+++ Hibernate-Orm/src/main/java/com/example/crud/pojo/TestData.java
@@ -0,0 +1,66 @@
+package com.example.crud.pojo;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "TESTDATA")
+public class TestData implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long dataId;
+	private String dataName;
+	private double dataValue;
+	
+	
+
+	public TestData() {
+		
+	}
+
+	public TestData(String dataName, double dataValue) {
+	
+		this.dataName = dataName;
+		this.dataValue = dataValue;
+	}
+	
+	public long getDataId() {
+		return dataId;
+	}
+	public void setDataId(long dataId) {
+		this.dataId = dataId;
+	}
+	public String getDataName() {
+		return dataName;
+	}
+	public void setDataName(String dataName) {
+		this.dataName = dataName;
+	}
+	public double getDataValue() {
+		return dataValue;
+	}
+	public void setDataValue(double dataValue) {
+		this.dataValue = dataValue;
+	}
+	
+	@Override
+	public String toString() {
+		return "TestData [dataName=" + dataName + ", dataValue=" + dataValue + "]";
+	}
+
+	
+	
+	
+	
+	
+	
+}
