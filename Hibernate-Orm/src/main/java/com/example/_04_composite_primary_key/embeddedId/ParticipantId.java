fccbd3a8f164391b4aa38904b06ef293ded39ea5
ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>
2021-12-29 15:53:07 +0300

    EmbeddedId

---
+++ Hibernate-Orm/src/main/java/com/example/_04_composite_primary_key/embeddedId/ParticipantId.java
@@ -0,0 +1,40 @@
+package com.example._04_composite_primary_key.embeddedId;
+
+import java.io.Serializable;
+
+public class ParticipantId implements Serializable{
+	
+	
+	/**
+	 * hsaglam
+	 */
+	private static final long serialVersionUID = 1L;
+	private long employeeId;
+	private long projectId;
+	
+	
+	public ParticipantId() {
+		
+	}
+	public long getEmployeeId() {
+		return employeeId;
+	}
+
+	public ParticipantId(long employeeId, long projectId) {
+		this.employeeId = employeeId;
+		this.projectId = projectId;
+	}
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+	public long getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(long projectId) {
+		this.projectId = projectId;
+	}
+
+	
+	
+
+}
