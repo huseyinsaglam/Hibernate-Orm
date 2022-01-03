f1f36924b2454280f361a6e0b56482c3afaf9e0e
ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>
2022-01-03 10:54:23 +0300

    embeddedId ile ilgili yorum satırı haline alındı sonradan kontrol edilip bakılacak

--- Hibernate-Orm/src/main/java/com/example/_04_composite_primary_key/embeddedId/Employee.java
+++ Hibernate-Orm/src/main/java/com/example/_04_composite_primary_key/embeddedId/Employee.java
@@ -1,49 +1,49 @@
-package com.example._04_composite_primary_key.embeddedId;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
-public class Employee {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long employeeId;
-	private String employeeName;
-	
-	public Employee() {
-		
-	}
-	
-	
-	public Employee(long employeeId, String employeeName) {
-		
-		this.employeeId = employeeId;
-		this.employeeName = employeeName;
-	}
-	public long getEmployeeId() {
-		return employeeId;
-	}
-	public void setEmployeeId(long employeeId) {
-		this.employeeId = employeeId;
-	}
-	public String getEmployeeName() {
-		return employeeName;
-	}
-	public void setEmployeeName(String employeeName) {
-		this.employeeName = employeeName;
-	}
-
-
-	@Override
-	public String toString() {
-		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + "]";
-	}
-	
-	
-	
-	
-
-}
+//package com.example._04_composite_primary_key.embeddedId;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//@Entity
+//public class Employee {
+//	
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+//	private long employeeId;
+//	private String employeeName;
+//	
+//	public Employee() {
+//		
+//	}
+//	
+//	
+//	public Employee(long employeeId, String employeeName) {
+//		
+//		this.employeeId = employeeId;
+//		this.employeeName = employeeName;
+//	}
+//	public long getEmployeeId() {
+//		return employeeId;
+//	}
+//	public void setEmployeeId(long employeeId) {
+//		this.employeeId = employeeId;
+//	}
+//	public String getEmployeeName() {
+//		return employeeName;
+//	}
+//	public void setEmployeeName(String employeeName) {
+//		this.employeeName = employeeName;
+//	}
+//
+//
+//	@Override
+//	public String toString() {
+//		return "Employee [employeeId=" + employeeId + ", employeeName=" + employeeName + "]";
+//	}
+//	
+//	
+//	
+//	
+//
+//}
--- Hibernate-Orm/src/main/java/com/example/_04_composite_primary_key/embeddedId/TestMain.java
+++ Hibernate-Orm/src/main/java/com/example/_04_composite_primary_key/embeddedId/TestMain.java
@@ -9,32 +9,32 @@ public class TestMain {
 
 	public static void main(String[] args) {
 
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("myPersistenceUnit");
-
-		EntityManager manager = factory.createEntityManager();
-		
-		Employee employee = new Employee(1,"employeeName");
-		Project project = new Project(1,"projectName");
-		
-		ParticipantId participantId = new ParticipantId(employee.getEmployeeId(), project.getProjectId());
-
-		Participant participant = new Participant();
-		participant.setParticipantId(participantId);
-		participant.setRoleName("admin");
-
-		
-
-		manager.getTransaction().begin();
-		manager.persist(employee);
-		manager.persist(project);
-		manager.getTransaction().commit();
-
-
-
-		manager.getTransaction().begin();
-		manager.persist(participant);
-		manager.getTransaction().commit();
-	    manager.close();
+//		EntityManagerFactory factory = Persistence.createEntityManagerFactory("myPersistenceUnit");
+//
+//		EntityManager manager = factory.createEntityManager();
+//		
+//		Employee employee = new Employee(1,"employeeName");
+//		Project project = new Project(1,"projectName");
+//		
+//		ParticipantId participantId = new ParticipantId(employee.getEmployeeId(), project.getProjectId());
+//
+//		Participant participant = new Participant();
+//		participant.setParticipantId(participantId);
+//		participant.setRoleName("admin");
+//
+//		
+//
+//		manager.getTransaction().begin();
+//		manager.persist(employee);
+//		manager.persist(project);
+//		manager.getTransaction().commit();
+//
+//
+//
+//		manager.getTransaction().begin();
+//		manager.persist(participant);
+//		manager.getTransaction().commit();
+//	    manager.close();
 	}
 
 }
