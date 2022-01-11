013b72c68b7303ad5809708eeab1115d11b6a9cc
ETIYACORP\huseyin.saglam <huseyin.saglam@etiya.com>
2022-01-11 16:49:56 +0300

    metamodel

--- Hibernate-Orm/src/main/java/com/example/_10_nativequery/TestMain.java
+++ Hibernate-Orm/src/main/java/com/example/_10_nativequery/TestMain.java
@@ -1,14 +1,12 @@
 package com.example._10_nativequery;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import javax.persistence.Query;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+
+import javax.persistence.metamodel.Attribute;
+import javax.persistence.metamodel.EntityType;
+import javax.persistence.metamodel.Metamodel;
 
 public class TestMain {
 
@@ -19,32 +17,15 @@ public class TestMain {
 			
 			EntityManager manager = factory.createEntityManager();
 		
+		
+			Metamodel metaModel = manager.getMetamodel();
+			EntityType<Example> getModel = metaModel.entity(Example.class);
 			
-			Example example1 = new Example(1,"exp1",10);
-
-			
-			Example example2 = new Example(2,"exp2",20);
-
-			
-			Example example3 = new Example(3,"exp3",30);
-
-			
-			
-			manager.getTransaction().begin();
-			manager.persist(example1);
-			manager.persist(example2);
-			manager.persist(example3);
-			manager.getTransaction().commit();
-			
-			String query = "select * from Example";
-			
-			Query datas=  manager.createNativeQuery(query,Example.class);
-			List<Example> testDatas =   datas.getResultList();
-			
-			for(Example data : testDatas) {
+			for(Attribute<?, ?> attributes :getModel.getAttributes() ) {
+				System.out.println(attributes.getName() +"-"+ attributes.getJavaType() +"\n");
 				
-				System.out.println(data);
 			}
+			
 			
 			manager.close();
 			
